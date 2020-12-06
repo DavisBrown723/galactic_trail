ca20b7907252e4f6bee9d9a334505c87f946d659
kasilve <73856496+kasilve@users.noreply.github.com>
2020-12-06 14:32:33 -0600

    messing with audio mixer and a settings menu

---
+++ Assets/__Scripts/MixerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class MixerController : MonoBehaviour
+{
+   
+}// end Class MixerController
--- Assets/__Scripts/SoundController.cs
+++ Assets/__Scripts/SoundController.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
     public AudioSource UIMusicPlayer;
-    public AudioSource GameMusicPlayer;
+     public AudioSource GameMusicPlayer;
+    [Header("UI")]
+    public AudioClip UIMenuMusic;
+
+
+    [Header("GAME")]
+    public AudioClip[] LoadScreenMusic;
+    public AudioClip[] LevelMusic;
+
+
+   
     }// end class SoundController 

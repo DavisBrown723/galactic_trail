4ac539a7cd43db837fb61e5e7e07ec6a1b8c5c28
kasilve <73856496+kasilve@users.noreply.github.com>
2020-12-06 11:52:39 -0600

    working on controlling sound for player

---
+++ Assets/__Scripts/SoundController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource UIMusicPlayer;
+    public AudioSource GameMusicPlayer;
+    }// end class SoundController 
